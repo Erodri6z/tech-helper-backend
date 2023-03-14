6a7d702630d058f9605b76505b1726c1991d2ec2
Erodri6z <erikrodriguez2417@gmail.com>
2023-03-13 19:13:02 -0500

    add a post model

---
+++ models/post.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema ({
+  text: StaticRange,
+  author: {type: Schema.Types.ObjectId, ref: 'Profile'}
+},{
+  timestamps: true
+})
+
+const postSchema = new Schema({
+  question: String,
+  elaboration: String,
+  comment: [commentSchema],
+  poster: {type: Schema.Types.ObjectId, ref: 'Profile'}
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+export{
+  Post
+}
